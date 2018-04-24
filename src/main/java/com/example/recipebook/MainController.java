41071c59d53105a30cf81132a8a9caaa3decce5a
Shannon Foster <shannon.fos3@outlook.com>
2018-04-24 11:17:27 -0400

    Add Thymeleaf fragments. Will add list and ids soon.

--- src/main/java/com/example/recipebook/MainController.java
+++ src/main/java/com/example/recipebook/MainController.java
@@ -1,28 +1,45 @@
 package com.example.recipebook;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/")
-    public String showIndex()
+    public String showIndex(Model model)
     {
+        model.addAttribute("menu", "home");
         return "index";
     }
 
     @RequestMapping("/recipes")
-    public String showRecipes()
+    public String showRecipes(Model model)
     {
+        model.addAttribute("menu", "recipes");
         return "recipelist";
     }
 
-    @RequestMapping("/wings")
-    public String wingsRecipe()
+    @RequestMapping("/recipes/wings")
+    public String wingsRecipe(Model model)
     {
-        return "wings";
+        model.addAttribute("menu", "/recipes/wings");
+        return "/recipes/wings";
     }
 
+    @RequestMapping("/recipes/injera")
+    public String injeraRecipe(Model model)
+    {
+        model.addAttribute("menu", "/recipes/injera");
+        return "/recipes/injera";
+    }
+
+    @RequestMapping("/recipes/pumpkinricenoodles")
+    public String pRNRecipe(Model model)
+    {
+        model.addAttribute("menu", "/recipes/pumpkinricenoodles");
+        return "/recipes/pumpkinricenoodles";
+    }
 
 }
